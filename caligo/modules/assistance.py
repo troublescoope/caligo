2cca76eeae8b384777d53eb88c1c2e6bca0cbad5
tomyprs <tomiprs25@gmail.com>
2023-09-26 22:01:24 +0700

    fix(afk): Missing import
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- caligo/modules/assistance.py
+++ caligo/modules/assistance.py
@@ -1,18 +1,17 @@
+from typing import ClassVar
+
 from caligo import command, module
+
 
 class Afk(module.Module):
     name: ClassVar[str] = "Afk"
-    
-    
+
     @command.desc("Handle message or tag when you're leaving")
     @command.usage("afk [reason?]")
     async def cmd_afk(self, ctx: command.Context):
         await ctx.respond("__Will be back later...__")
-        
-        
+
     @command.desc("Remove afk handler")
     @command.usage("unafk")
     async def cmd_unafk(self, ctx: command.Context):
         await ctx.respond("You're back\n\n You can view logging in bot")
-        
-        
