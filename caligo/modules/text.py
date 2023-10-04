454c58f3bdf08d0e3dd218d42b88c91ea05073bf
tomyprs <tomiprs25@gmail.com>
2023-10-04 08:06:06 +0700

    feat(text): Add handling for empty input in 'Text' module
    This change adds a check for empty input in the 'Text' module's logic. If no input is provided, it now returns "__No input found..__". This enhancement improves the module's usability and provides better feedback when no input is given.
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- caligo/modules/text.py
+++ caligo/modules/text.py
@@ -134,6 +134,9 @@ class Text(module.Module):
         else:
             content = ctx.input
 
+        if not content:
+            return "__No input found..__"
+
         headers = {
             "Accept-Language": "id-ID",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
