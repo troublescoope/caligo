0ad0150eb5179e700a4da7608d26e53e64a33dfc
tomyprs <tomiprs25@gmail.com>
2024-03-16 02:25:50 +0700

    feat(transmission): add async add_download function
    Introduces an asynchronous function, add_download, to the transmission module. This function enables the addition of downloads from Telegram messages asynchronously. It enhances the module's functionality by allowing users to initiate downloads in an asynchronous manner, improving performance and responsiveness.
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- caligo/modules/transmission.py
+++ caligo/modules/transmission.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from typing import Any, ClassVar, Literal, Optional, Set, Tuple
 
 from aiopath import AsyncPath
+from pyrogram import types
 
 from caligo import command, module, util
 
@@ -15,6 +16,9 @@ async def prog_func(
     ctx: command.Context,
     file_name: str,
 ) -> None:
+    """
+    Progress function to display upload/download progress.
+    """
     percent = current / total
     end_time = util.time.sec() - start_time
     now = datetime.now()
@@ -36,7 +40,8 @@ async def prog_func(
         f"`{file_name}`\n"
         f"Status: **{status}**\n"
         f"Progress: [{bullets + space}] {round(percent * 100)}%\n"
-        f"__{util.misc.human_readable_bytes(current)} of {util.misc.human_readable_bytes(total)} @ "
+        f"__{util.misc.human_readable_bytes(current)} of "
+        f"{util.misc.human_readable_bytes(total)} @ "
         f"{util.misc.human_readable_bytes(speed, postfix='/s')}\n"
         f"eta - {util.time.format_duration_td(eta)}__\n\n"
     )
@@ -47,7 +52,6 @@ async def prog_func(
         or (now - ctx.last_update_time).total_seconds() >= 5
     ):
         await ctx.respond(progress)
-
         ctx.last_update_time = now
 
 
@@ -59,82 +63,52 @@ class Transmission(module.Module):
     async def on_load(self) -> None:
         self.tasks = set()
 
-    @command.desc("Abort transmission of upload or download")
-    @command.usage("[message progress to abort]", reply=True)
-    async def cmd_abort(self, ctx: command.Context) -> Optional[str]:
-        if not ctx.input and not ctx.msg.reply_to_message:
-            return "__Pass GID or reply to message of task to abort transmission.__"
-
-        if ctx.msg.reply_to_message and ctx.input:
-            return "__Can't pass GID/Message Id while replying to message.__"
-
-        reply_msg = ctx.msg.reply_to_message
-
-        for msg_id, task in list(self.tasks.copy()):
-            if reply_msg and reply_msg.id == msg_id or ctx.input == int(msg_id):
-                task.cancel()
-                self.tasks.remove((msg_id, task))
-                break
-            else:
-                return "__The message you choose is not in task.__"
-
-        await ctx.msg.delete()
-
-    @command.desc("Download file from telegram server")
-    @command.alias("dl")
-    @command.usage("download reply or send link with [message media to download]")
-    async def cmd_download(self, ctx: command.Context) -> str:
-        reply_msg = ctx.msg.reply_to_message
-        if reply_msg and not reply_msg.media:
-            return "__The message you replied to doesn't contain any media.__"
-
+    async def download_media(
+        self,
+        ctx: command.Context,
+        message: types.Message,
+        task_name: str,
+        all_media: bool = False,
+    ) -> Optional[str]:
+        """
+        Download media from a Telegram message.
+        """
         start_time = util.time.sec()
 
-        await ctx.respond("Preparing to download...")
-
-        # Check if media is group or not
-        if ctx.input:
-            chat_id, msg_id = await util.tg.parse_telegram_link(ctx.input)
+        if not all_media:
+            if not message.media:
+                return "__Failed to download media: No media found.__"
+            media = message.media
+            msg_list = [message]
         else:
-            chat_id = ctx.chat.id
-            msg_id = reply_msg.id
-
-        message = (
-            reply_msg
-            if reply_msg
-            else await self.bot.client.get_messages(chat_id, msg_id)
-        )
-        try:
-            media_group = await self.bot.client.get_media_group(chat_id, msg_id)
-        except ValueError:
-            media_group = []
-            media_group.append(message)
+            try:
+                msg_list = await self.bot.client.get_media_group(
+                    message.chat.id, message.id
+                )
+            except ValueError:
+                msg_list = [message]
 
         results = set()
-        for msg in media_group:
+
+        for msg in msg_list:
             media = getattr(msg, msg.media.value)
             if not media:
-                return "Media is empty!, maybe group can't display in your country?"
+                continue
+
             try:
                 name = media.file_name
             except AttributeError:
-                if hasattr(media, "date"):
-                    name = (
-                        f"{msg.media.value}_{media.date.strftime('%Y-%m-%d_%H-%M-%S')}"
-                    )
-                else:
-                    name = f"{msg.media.value}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+                name = (
+                    f"{msg.media.value}_{media.date.strftime('%Y-%m-%d_%H-%M-%S')}"
+                    if hasattr(media, "date")
+                    else f"{msg.media.value}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+                )
 
             task = self.bot.loop.create_task(
                 self.bot.client.download_media(
                     msg,
                     progress=prog_func,
-                    progress_args=(
-                        start_time,
-                        "download",
-                        ctx,
-                        name,
-                    ),
+                    progress_args=(start_time, "download", ctx, name),
                 )
             )
             self.tasks.add((ctx.msg.id, task))
@@ -154,20 +128,77 @@ class Transmission(module.Module):
 
             if isinstance(result, str):
                 path += (
-                    f"\n× `{self.bot.client.workdir}/downloads/{result.split('/')[-1]}`"
+                    f"\n× `{ctx.bot.client.workdir}/downloads/{result.split('/')[-1]}`"
                 )
             else:
-                path += f"\n× `{self.bot.client.workdir}/downloads/{result.name}`"
+                path += f"\n× `{ctx.bot.client.workdir}/downloads/{result.name}`"
 
         if not path:
             return "__Failed to download media.__"
 
         return f"Downloaded to:\n{path}"
 
+    @command.desc("Abort transmission of upload or download")
+    @command.usage("[message progress to abort]", reply=True)
+    async def cmd_abort(self, ctx: command.Context) -> Optional[str]:
+        """
+        Abort transmission task.
+        """
+        if not ctx.input and not ctx.msg.reply_to_message:
+            return "__Pass GID or reply to message of task to abort transmission.__"
+
+        if ctx.msg.reply_to_message and ctx.input:
+            return "__Can't pass GID/Message Id while replying to message.__"
+
+        reply_msg = ctx.msg.reply_to_message
+
+        for msg_id, task in list(self.tasks.copy()):
+            if reply_msg and reply_msg.id == msg_id or ctx.input == int(msg_id):
+                task.cancel()
+                self.tasks.remove((msg_id, task))
+                break
+            else:
+                return "__The message you choose is not in task.__"
+
+        await ctx.msg.delete()
+
+    @command.desc("Download file from telegram server.")
+    @command.alias("dl")
+    @command.usage(
+        "download reply or send link with [message media to download]\n-b: Bulk media download."
+    )
+    async def cmd_download(self, ctx: command.Context) -> str:
+        """
+        Download file from a Telegram message.
+        """
+        reply_msg = ctx.msg.reply_to_message
+        if reply_msg and not reply_msg.media:
+            return "__The message you replied to doesn't contain any media.__"
+
+        await ctx.respond("Preparing to download...")
+
+        if ctx.input:
+            chat_id, msg_id = await util.tg.parse_telegram_link(ctx.input)
+        else:
+            chat_id = ctx.chat.id
+            msg_id = reply_msg.id
+
+        message = (
+            reply_msg
+            if reply_msg
+            else await self.bot.client.get_messages(chat_id, msg_id)
+        )
+        bulk = True if "b" in ctx.flags else False
+
+        return await self.download_media(ctx, message, "Download", bulk)
+
     @command.desc("Upload file into telegram server")
     @command.alias("ul")
     @command.usage("[file path]")
     async def cmd_upload(self, ctx: command.Context) -> Optional[str]:
+        """
+        Upload file to a Telegram server.
+        """
         if not ctx.input:
             return "__Pass the file path.__"
 
@@ -178,7 +209,7 @@ class Transmission(module.Module):
             return "__The path you input is a directory.__"
 
         if not await file_path.is_file():
-            return "__The file you input doesn't exists.__"
+            return "__The file you input doesn't exist.__"
 
         await ctx.respond("Preparing to upload...")
         task = self.bot.loop.create_task(
@@ -188,12 +219,7 @@ class Transmission(module.Module):
                 message_thread_id=ctx.msg.message_thread_id,
                 force_document=True,
                 progress=prog_func,
-                progress_args=(
-                    start_time,
-                    "upload",
-                    ctx,
-                    file_path.name,
-                ),
+                progress_args=(start_time, "upload", ctx, file_path.name),
             )
         )
         self.tasks.add((ctx.msg.id, task))
