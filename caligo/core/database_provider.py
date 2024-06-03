e13906ca0f512f1b759dc0bc0dd3d4f04959539b
tomyprs <tomiprs25@gmail.com>
2024-06-03 15:47:19 +0700

    feat(database): add DNS configuration for database connection
    - Added DNS resolver import (`import dns.resolver`) in `database_provider.py`.
    - Introduced DNS configuration handling in `DatabaseProvider.__init__` method.
    - Updated `sample_config.toml` to include `db_dns` configuration option.
    
    The changes were implemented to allow custom DNS resolution for the database connection. This is particularly useful for environments where DNS settings need to be specified explicitly rather than relying on system defaults.
    
    1. **Importing DNS Resolver:** The `dns.resolver` import enables DNS resolution capabilities within the `database_provider.py` module.
    
    2. **Configuring DNS Resolver:**
       - **Retrieving DNS Configuration:** The code checks if the `db_dns` configuration is provided in the configuration file.
       - **Setting DNS Resolver:** If DNS addresses are provided, the default DNS resolver is reconfigured to use these specified DNS servers, ensuring that the database connection resolves through the intended DNS servers.
    
    3. **Updating Configuration File:** The `sample_config.toml` file is updated to include an example `db_dns` entry, guiding users on how to set custom DNS servers for the database connection.
    
    These changes enhance the flexibility and reliability of database connections in diverse network environments, ensuring that DNS resolution can be explicitly controlled and customized.
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

--- caligo/core/database_provider.py
+++ caligo/core/database_provider.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING, Any
 
+import dns.resolver
+
 from .base import CaligoBase
 from .database import AsyncClient, AsyncDatabase
 
@@ -11,6 +13,12 @@ class DatabaseProvider(CaligoBase):
     db: AsyncDatabase
 
     def __init__(self: "Caligo", **kwargs: Any) -> None:
+        # Check if DNS configuration is provided and has value
+        db_dns: list = [self.config["bot"].get("db_dns")]
+        if db_dns:
+            dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+            dns.resolver.default_resolver.nameservers = db_dns
+
         client = AsyncClient(self.config["bot"]["db_uri"], connect=False)
         self.db = client.get_database("CALIGO")
 
